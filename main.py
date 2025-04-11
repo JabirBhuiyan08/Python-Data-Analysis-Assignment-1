e6bc90a28fa852f7c00a826bd17e1cdee59000cd
Jabir Bhuiyan <jabirbhuiyan08@gmail.com>
2025-04-11 14:56:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-# 1. ভ্যারিয়েবল ও ডাটা টাইপ:
+# 1. Variable and Data Type:
 Name = "Jabir Bhuiyan"
 Age = 24
 is_Student = True
 
-# 2. অ্যারিথমেটিক অপারেশন:
+# 2. Arithmetic Operations:
 print("Name:", Name, "Age:", Age, "Student:", is_Student)
 print(type(Name), type(Age), type(is_Student))
 
 
-# 3. কম্পারিজন অপারেটর:
+# 3. Comparison Oparetor:
 multiple = Age*2
 plus = Age+3
 minus = Age-5
 print("Multiple:", multiple, "Plus:", plus, "Minus:", minus)
 
-# 4. লজিক্যাল অপারেটর:
+# 4. Logical Operator:
 print(Age >= 18)
 print(Age >= 18 and Age == 30)
 
-# 5. অ্যাসাইনমেন্ট অপারেটর:
+# 5. Assignment Operator:
 has_student_id = True
 
 print("Has ID and student?", is_Student and has_student_id)
 print("Has ID or permission?", has_student_id or is_Student)
 print("Not a student?", not is_Student)
 
-# 6. আইডেন্টিটি অপারেটর:
+# 6. Identity Operator:
 assignment_mark = 50
 print(assignment_mark)
 assignment_mark += 10
@@ -43,7 +43,7 @@ print(assignment_mark)
 assignment_mark %= 3
 print(assignment_mark)
 
-# 7. মেম্বারশিপ অপারেটর:
+# 7. Membership Operator:
 map=["Dhaka", "Chittagong", "Khulna", "Rajshahi", "Noakhali"]
 print("Dhaka" in map)
 print("Khulna" in map)
